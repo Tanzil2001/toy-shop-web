d59c200d496ea2ce92774f77ad8d9491be07541a
Mahin Jasim <barakattanzil@gmail.comm>
2023-05-17 23:20:43 +0600

    navbar done

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../Shared/Footer";
+import MyNavbar from "../Shared/MyNavbar";
 
 const MainLayout = () => {
     return (
         <div>
+            <MyNavbar></MyNavbar>
             <Outlet></Outlet>
             <Footer></Footer>
         </div>
