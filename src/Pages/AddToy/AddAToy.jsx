269eca7690e9614e88fe27242a4cab6195b861e6
Mahin Jasim <barakattanzil@gmail.comm>
2023-05-18 21:01:36 +0600

    add toy done

--- src/Pages/AddToy/AddAToy.jsx
+++ src/Pages/AddToy/AddAToy.jsx
@@ -1,17 +1,54 @@
+import { useContext } from "react";
+import { AuthContext } from "../../Provider/AuthProvider";
 
 const AddAToy = () => {
+
+    const { user } = useContext(AuthContext);
+
+
+    const handlePost = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const toyName = form.toyName.value;
+        const sellerName = form.name.value;
+        const toyPhoto = form.toyPhoto.value;
+        const sellerEmail = form.email.value;
+        const subCategory = form.subCategory.value;
+        const price = form.price.value;
+        const rating = form.rating.value;
+        const quantity = form.quantity.value;
+        const details = form.details.value;
+
+        const addToy = { toyName, sellerName, sellerEmail, toyPhoto, subCategory, price, rating, quantity, details };
+        console.log(addToy);
+
+        fetch('http://localhost:5000/addtoys', {
+            method: 'POST',
+            headers:{
+                'content-type':'application/json'
+            },
+            body:JSON.stringify(addToy)
+        })
+        .then(result =>{
+            console.log(result);
+        })
+        .then(data => {
+            console.log(data);
+        })
+    }
+
     return (
         <div className="my-8 ">
-            <form className="grid grid-cols-2 gap-5 ml-32">
-                <input className="p-3 w-2/3 bg-slate-200 rounded-md" type="text" name="name" placeholder="Toy Name"/>
-                <input className="p-3 w-2/3 bg-slate-200 rounded-md" type="url" name="toyPhoto" placeholder="Toy Photo"/>
-                <input className="p-3 w-2/3 bg-slate-200 rounded-md" type="text" name="name" placeholder="Seller Name"/>
-                <input className="p-3 w-2/3 bg-slate-200 rounded-md" type="email" name="email" placeholder="Seller Email"/>
-                <input className="p-3 w-2/3 bg-slate-200 rounded-md" type="text" name="subCategory" placeholder="Sub Category"/>
-                <input className="p-3 w-2/3 bg-slate-200 rounded-md" type="number" name="price" placeholder="Price"/>
-                <input className="p-3 w-2/3 bg-slate-200 rounded-md" type="number" name="rating" placeholder="Rating"/>
-                <input className="p-3 w-2/3 bg-slate-200 rounded-md" type="number" name="quantity" placeholder="Available quantity"/>
-                <input className="p-3 w-2/3 bg-slate-200 rounded-md" type="text" name="details" placeholder="Detail description"/>
+            <form onSubmit={handlePost} className="grid grid-cols-2 gap-5 ml-32">
+                <input className="p-3 w-2/3 bg-slate-200 rounded-md" type="text" name="toyName" placeholder="Toy Name" />
+                <input className="p-3 w-2/3 bg-slate-200 rounded-md" type="url" name="toyPhoto" placeholder="Toy Photo" />
+                <input className="p-3 w-2/3 bg-slate-200 rounded-md" type="text" value={user?.displayName} name="name" placeholder="Seller Name" />
+                <input className="p-3 w-2/3 bg-slate-200 rounded-md" type="email" value={user?.email} name="email" placeholder="Seller Email" />
+                <input className="p-3 w-2/3 bg-slate-200 rounded-md" type="text" name="subCategory" placeholder="Sub Category" />
+                <input className="p-3 w-2/3 bg-slate-200 rounded-md" type="number" name="price" placeholder="Price" />
+                <input className="p-3 w-2/3 bg-slate-200 rounded-md" type="number" name="rating" placeholder="Rating" />
+                <input className="p-3 w-2/3 bg-slate-200 rounded-md" type="number" name="quantity" placeholder="Available quantity" />
+                <input className="p-3 w-2/3 bg-slate-200 rounded-md" type="text" name="details" placeholder="Detail description" />
                 <input className="p-3 w-2/3 bg-slate-200 rounded-md" type="submit" value="Add Toy" />
             </form>
         </div>
